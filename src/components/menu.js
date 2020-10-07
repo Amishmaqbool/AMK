63e817a50b7fab2886559f7b2194ae81f122f401
Amishmaqbool <Amishmaqbool@gmail.com>
2020-10-08 02:56:02 +0500

    cv added

--- src/components/menu.js
+++ src/components/menu.js
@@ -263,7 +263,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a target="_blank" href="/resume.pdf" className="resume-link">
               Resume
             </a>
           </nav>
